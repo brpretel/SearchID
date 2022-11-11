a17eab3a704a26715ba6d038b0d572c6cf082618
ivan0627 <santiagog0627@gmail.com>
2022-11-11 17:05:10 -0500

    Third commit, making layouts workable

---
+++ SearchID/app/src/main/java/com/example/searchid/activity_main.java
@@ -0,0 +1,76 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		frame_3
+	 *	@date 		Friday 11th of November 2022 04:49:31 PM
+	 *	@title 		Login
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.searchid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class activity_main extends Activity {
+
+	
+	private View _bg__frame_1_ek2;
+	private View rectangle3;
+	private View rectangle1_ek1;
+	private TextView signin;
+	private View line2_ek2;
+	private ImageView vector_ek20;
+	private ImageView vector_ek21;
+	private ImageView vector_ek22;
+	private ImageView vector_ek23;
+	private View line2_ek3;
+	private ImageView vector_ek24;
+	private ImageView vector_ek25;
+	private View rectangle2_ek2;
+	private TextView signin_ek1;
+	private TextView forgotpassword;
+	private TextView forgotpassword_ek1;
+	private ImageView logo_ek1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		
+		_bg__frame_1_ek2 = (View) findViewById(R.id._bg__frame_1_ek2);
+
+		rectangle1_ek1 = (View) findViewById(R.id.rectangle1_ek1);
+		signin = (TextView) findViewById(R.id.signin);
+		vector_ek20 = (ImageView) findViewById(R.id.vector_ek20);
+		vector_ek21 = (ImageView) findViewById(R.id.vector_ek21);
+		vector_ek22 = (ImageView) findViewById(R.id.vector_ek22);
+		vector_ek23 = (ImageView) findViewById(R.id.vector_ek23);
+		vector_ek24 = (ImageView) findViewById(R.id.vector_ek24);
+		vector_ek25 = (ImageView) findViewById(R.id.vector_ek25);
+		rectangle2_ek2 = (View) findViewById(R.id.btn_submit);
+		forgotpassword = (TextView) findViewById(R.id.btn_forgotpassword);
+		logo_ek1 = (ImageView) findViewById(R.id.logo_ek1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
